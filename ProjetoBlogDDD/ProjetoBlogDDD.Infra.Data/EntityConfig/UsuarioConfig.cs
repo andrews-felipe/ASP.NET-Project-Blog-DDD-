9f483d22e9acfb0ee8389b8a9d03a4c1aa587352
andrews-fs <ferulip@gmail.com>
2017-11-20 01:28:01 -0200

    Request de Entidades / Config Db

---
+++ ProjetoBlogDDD/ProjetoBlogDDD.Infra.Data/EntityConfig/UsuarioConfig.cs
@@ -0,0 +1,25 @@
+﻿
+using ProjetoBlogDDD.Dominio.Entidades;
+using System.Data.Entity.ModelConfiguration;
+
+namespace ProjetoBlogDDD.Infra.Data.EntityConfig
+{
+    public class UsuarioConfig : EntityTypeConfiguration<Usuario>
+    {
+
+        public UsuarioConfig()
+        {
+            HasKey(u => u.usuarioID);
+
+            Property(u => u.Nome)
+                .IsRequired()
+                .HasMaxLength(150);
+
+            Property(u => u.Email)
+                .IsRequired();
+                
+                
+
+        }
+    }
+}

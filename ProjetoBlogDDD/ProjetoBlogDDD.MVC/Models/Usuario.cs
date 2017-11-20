4df2e662c7ed7d4c1110c5a53b17c44a84aa2f64
andrews-fs <ferulip@gmail.com>
2017-11-20 11:13:01 -0200

    Criando restrições de Entidades

---
+++ ProjetoBlogDDD/ProjetoBlogDDD.MVC/Models/Usuario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ProjetoBlogDDD.MVC.Models
+{
+    public class Usuario
+    {   
+
+        [Key]
+
+        public int usuarioID { get; set; }
+
+        [Required(ErrorMessage ="Prencha Campo Nome")]
+        [MaxLength(150, ErrorMessage = "Limite Máximo Excedido")]
+        [MinLength(2, ErrorMessage ="Digite O Minimo de Caracteres")]
+
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Prencha Campo Nome")]
+        [MaxLength(150, ErrorMessage = "Limite Máximo Excedido")]
+        [MinLength(2, ErrorMessage = "Digite O Minimo de Caracteres")]
+
+        public string Email { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        //public virtual IEnumerable<Post> Postagens { get; set; }//
+    }
+}
